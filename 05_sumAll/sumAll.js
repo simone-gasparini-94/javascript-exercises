7c987e7263eb91878c5f7fe6842f02dd4ff8f05d
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2025-01-13 18:33:11 +0100

    05 -- All tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,6 @@
 const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    if (num1 < 0 || num2 < 0) return "ERROR";
     let sum = 0;
     if(num1 > num2){
         for(i = num2; i <= num1; i++) {
