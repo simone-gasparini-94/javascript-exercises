ff9c25576f7c665566dceaea9886478a1c370efd
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2025-01-13 14:59:06 +0100

    02 -- All tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    if (times < 0) {
+        return "ERROR";
+    }
+    let newString = "";
+    for (i = 0; i < times; i++) {
+        newString += string;
+    }
+    return newString;
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
