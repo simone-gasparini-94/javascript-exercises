10403eee71eca4f26ca7996b95f72caca349f0df
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2025-01-13 17:12:25 +0100

    03 -- All tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = string.split("");
+    let reverseArray = array.reverse();
+    let reverseString = reverseArray.join("");
+    return reverseString;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
