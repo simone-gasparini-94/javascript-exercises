bd98e130be7265d72e219a12e08eff5feb155693
simone-gasparini-94 <simone.gasparini.2@gmail.com>
2025-01-14 11:23:29 +0100

    07 -- All tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsius = (fahrenheit - 32) / 1.8;
+  if(!Number.isInteger(celsius)) return +celsius.toFixed(1);
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = celsius * 1.8 + 32;
+  if(!Number.isInteger(fahrenheit)) return +fahrenheit.toFixed(1);
+  return fahrenheit;
 };
 
 // Do not edit below this line
